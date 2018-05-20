b88aff448edb3ad0a850c75ae31810e55ea0694c
jeffersonaugusto140 <jefferson.augusto140@gmail.com>
2018-05-20 12:48:14 -0300

    Iniciando jokempo

--- app1/index.js
+++ app1/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, Text, Alert, Button, View, StyleSheet } from 'react-native';
 
 class CustomCmp extends Component {
     render() {
@@ -7,20 +7,78 @@ class CustomCmp extends Component {
             <View>
                 <Text>
                     {this.props.p1}
-            </Text>
-                <Text>
-                    {this.props.p2}
-            </Text>
+                </Text>
             </View>
         );
     }
 }
 
 class App1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userValue: '',
+            cpuValue: '',
+            resultado: '',
+            values: ['Pedra', 'Papel', 'Tesoura']
+        };
+    }
+
+    styleBtn = {
+        btn: {
+            justifyContent: 'space-around',
+            alignItems: 'flex-start',
+            flexDirection: 'row',
+            marginBottom: 20
+        }
+    }
+
+    jokenpo(value) {
+        let cpu = this.state.values[Math.floor(Math.random() * 3)];
+        let res = '';
+
+        userWin = (user, cpu, values) => {
+            let pedra = values[0];
+            let papel = values[1];
+            let tesoura = values[2];
+
+            ret = (user === pedra && cpu === tesoura) ||
+                (user === papel && cpu === pedra) ||
+                (user === tesoura && cpu === papel);
+
+            return ret;
+
+        };
+
+        if (cpu === value) {
+            res = 'Empate';
+        } else if (userWin(value, cpu, this.state.values)) {
+            res = 'Você ganhou';
+        } else {
+            res = 'Você perdeu... kkkkkk...';
+        }
+
+        this.setState({
+            userValue: value,
+            cpuValue: cpu,
+            resultado: res,
+            values: this.state.values
+        });
+    }
+
     render() {
         return (
-            <CustomCmp p1='AAAA' p2='BBBB'></CustomCmp>
-        )
+            <View>
+                <View style={this.styleBtn.btn}>
+                    <Button title={this.state.values[0]} onPress={() => { this.jokenpo(this.state.values[0]); }} />
+                    <Button title={this.state.values[1]} onPress={() => { this.jokenpo(this.state.values[1]); }} />
+                    <Button title={this.state.values[2]} onPress={() => { this.jokenpo(this.state.values[2]); }} />
+                </View>
+                <Text> User value: {this.state.userValue} </Text>
+                <Text> CPU value: {this.state.cpuValue} </Text>
+                <Text> Resultado: {this.state.resultado} </Text>
+            </View>
+        );
     }
 }
 
