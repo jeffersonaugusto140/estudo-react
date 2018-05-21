fb588c3263e85cd8c35cc96407864738922a2ac8
jeffersonaugusto140 <jefferson.augusto140@gmail.com>
2018-05-20 21:40:20 -0300

    Configurado eslint e adicionado arquivos da listagem de itens

---
+++ app4/src/components/listagemItens/Itens.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Item extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
---
+++ app4/src/components/listagemItens/ListaItens.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class ListaItens extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
