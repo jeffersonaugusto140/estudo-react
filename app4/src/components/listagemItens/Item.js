53057f4a1c2ffba97da78fe3e7029380de28aa32
jeffersonaugusto140 <jefferson.augusto140@gmail.com>
2018-05-25 20:37:53 -0300

    Finalizado app4

--- app4/src/components/listagemItens/Item.js
+++ app4/src/components/listagemItens/Item.js
@@ -1,11 +1,58 @@
-
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        margin: 5,
+        borderWidth: 0.5,
+        borderColor: '#999',
+        padding: 10,
+        backgroundColor: 'white'
+    },
+    viewImage: {
+        fontSize: 16
+    },
+    viewItems: {
+        marginLeft: 20,
+        flex: 1
+    },
+    stImage: {
+        height: 100,
+        width: 100
+    },
+    stTitulo: {
+        fontSize: 18,
+        color: 'blue',
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    stValor: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+});
 
 export default class Item extends Component {
+
     render() {
+        const { foto, titulo, valor, local_anuncio, data_publicacao } = this.props.itemProps;
+        const { viewContainer, viewItems, viewImage, stImage, stTitulo, stValor } = styles;
+        console.log('viewContainer');
+        console.log(viewContainer);
         return (
-            <Text>Test 000</Text>
+            <View style={viewContainer}>
+                <View style={viewImage}>
+                    <Image style={stImage} source={{ uri: foto }} />
+                </View>
+                <View style={viewItems}>
+                    <Text style={stTitulo}>{titulo}</Text>
+                    <Text style={stValor}>R$ {valor}</Text>
+                    <Text>Local: {local_anuncio}</Text>
+                    <Text>Publicado no dia: {data_publicacao}</Text>
+                </View>
+            </View>
         );
     }
 }
--- app4/src/components/listagemItens/ListaItens.js
+++ app4/src/components/listagemItens/ListaItens.js
@@ -1,27 +1,36 @@
-
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import axios from 'axios';
 import Item from './Item';
 
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: '#DDD'
+    }
+});
+
 export default class ListaItens extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+
     componentWillMount() {
         const uri = 'http://faus.com.br/recursos/c/dmairr/api/itens.html';
         axios.get(uri)
             .then((response) => {
-                console.log('Aeeewwwwwwwwwwww....');
                 console.log(response);
+                this.setState({ data: response.data });
             })
             .catch(() => { console.log('error.'); });
     }
 
     render() {
         return (
-            <View>
-                <Item />
-                <Item />
-                <Item />
-            </View>
+            <ScrollView style={styles.scrollView}>
+                { this.state.data.map(d => (<Item key={d.titulo} itemProps={d} />)) }
+            </ScrollView>
         );
     }
 }
